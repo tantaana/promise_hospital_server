d1e0206302b1611e61ef583d89d0aaa594c7f638
Fouzia Tabassum <tabassumfouzia13@gmail.com>
2023-02-03 19:28:12 +0600

    update service

--- routes/NameOne/nameOne.js
+++ routes/NameOne/nameOne.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const multer =require("multer")
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
@@ -23,6 +24,13 @@ const run = async () => {
             const categories = await categoriesCollection.find(query).toArray()
             res.send(categories)
         })
+        router.post('/specialities', async (req, res) => {
+            const specialities = req.body;
+            specialities.createdAt = new Date();
+            console.log(specialities);
+            const result = await specialitiesCollection.insertOne(specialities)
+            res.send(result);
+        })
         router.get('/specialities', async (req, res) => {
             const query = {};
             const specialities = await specialitiesCollection.find(query).toArray()
@@ -33,6 +41,14 @@ const run = async () => {
             const query = {_id:ObjectId(id)};
             const specialities = await specialitiesCollection.findOne(query)
             res.send(specialities)
+        })
+        router.delete('/specialities/:id', async (req, res) => {
+            console.log(req)
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await specialitiesCollection.deleteOne(query);
+            console.log(result);
+            res.send(result);
         })
     }
     finally {
