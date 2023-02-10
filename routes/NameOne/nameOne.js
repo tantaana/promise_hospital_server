d5b5226ac7c4cdbb40c94ac84fd00b7af8ea9dfe
Fouzia Tabassum <tabassumfouzia13@gmail.com>
2023-02-10 17:30:34 +0600

    5th update

--- routes/NameOne/nameOne.js
+++ routes/NameOne/nameOne.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const multer =require("multer")
+const server = require("http").Server(express)
+const io = require('socket.io')(server)
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { v4:uuidV4 } =require('uuid')
 
+// router.set('view engin ', 'ejs')
+// router.use(express.static('public'))
 
-
+// server.listen(3000)
 
 const uri = `mongodb+srv://endgame:${process.env.DB_PASSWORD}@cluster0.flakcz3.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -17,8 +21,8 @@ const run = async () => {
     try {
         const categoriesCollection = client.db('categories').collection('category')
         const specialitiesCollection = client.db('Promise_hospital').collection('specialities')
-        
-        
+        const docInfo = client.db('Promise_hospital').collection('docInfo');
+
         router.get('/categories', async (req, res) => {
             const query = {};
             const categories = await categoriesCollection.find(query).toArray()
@@ -38,7 +42,7 @@ const run = async () => {
         })
         router.get('/specialities/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id:ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const specialities = await specialitiesCollection.findOne(query)
             res.send(specialities)
         })
@@ -50,6 +54,19 @@ const run = async () => {
             console.log(result);
             res.send(result);
         })
+        router.get('/docInfo', async (req, res) => {
+            const query = {};
+            const options = await docInfo.find(query).toArray();
+            res.send(options)
+        })
+
+//         router.get('/', (req, res) => {
+//             res.redirect(`/${uuidV4()}`)
+//         })
+
+//         router.get('/:room', (req, res) => {
+// res.render('room',{roomId:req.params.room})
+//         })
     }
     finally {
 
@@ -64,8 +81,8 @@ run().catch(err => console.log(err))
 
 
 
-router.get('/nameOne',(req,res)=>{
-res.send('nameOne')
+router.get('/nameOne', (req, res) => {
+    res.send('nameOne')
 })
 
 module.exports = router;
