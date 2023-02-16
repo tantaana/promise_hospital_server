a51744c3c23a258ebd0c70079894b49c8b1e495d
tantaana <shariartanvirofficial@gmail.com>
2023-02-16 14:03:45 +0600

    another

--- routes/nameThree/nameThree.js
+++ routes/nameThree/nameThree.js
@@ -20,6 +20,7 @@ const run = async () => {
 
         const userData = client.db('Promise_hospital').collection('userData');
         const docInfo = client.db('Promise_hospital').collection('docInfo');
+        const newSpecialities = client.db('Promise_hospital').collection('newSpecialities');
 
         // ------------------------
         router.post('/createAppointment', async (req, res) => {
@@ -40,25 +41,63 @@ const run = async () => {
             const email = user.email;
             const query = { email: email };
             const find = await userData.findOne(query);
-
             if (find === null) {
                 const insertData = await userData.insertOne(user);
                 res.send(insertData)
+            }
+            else {
+                return console.log('hello');
             }
 
         })
 
         router.get('/userData', async (req, res) => {
             const query = {};
-            const options = await userData.find(query).toArray();
+            const options = await userData.find(query, { name: 1, _id: 0 }).sort({ "name": 1 }).toArray();
             res.send(options)
+        });
+
+        router.put('/userData/:id', async (req, res) => {
+            // const decodedEmail = req.decoded.email;
+            // const query = { email: decodedEmail }
+            // const user = await usersCollections.findOne(query)
+            // if (user?.role !== 'admin') {
+            //     return res.status(403).send({ message: 'forbidden access ' })
+            // }
+
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true }
+            const updatedDoc = {
+                $set: {
+                    userType: 'doctor'
+                }
+            }
+            const result = await userData.updateOne(filter, updatedDoc, options)
+            res.send(result)
+
+        });
+
+        router.delete('/userData/deletedData/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const result = await userData.deleteOne(filter)
+            res.send(result)
         })
+
+        // router.get('/emailData', async (req, res) => {
+        //     const email = req.query.email;
+        //     const query = { email: email };
+        //     const result = await userData.find(query).toArray();
+        //     res.send(result)
+        // })
 
         // ----------------------------------
 
         router.get('/docInfo', async (req, res) => {
             const query = {};
-            const options = await docInfo.find(query).toArray();
+            // const options = await docInfo.find(query).toArray();
+            const options = await docInfo.find(query, { name: 1, _id: 0 }).sort({ "name": 1 }).toArray();
             res.send(options)
         })
 
@@ -94,6 +133,22 @@ const run = async () => {
             const result = await appointmentData.findOne(query);
             res.send(result)
         })
+
+        //------------------------------
+
+        router.get('/specialities', async (req, res) => {
+            const query = {};
+            const options = await newSpecialities.find(query, { speciality: 1, _id: 0 }).sort({ "speciality": 1 }).toArray();
+            res.send(options)
+        });
+
+        router.get('/specialities/:speciality_id', async (req, res) => {
+            const speciality_id = req.params.speciality_id;
+            const query = { speciality_id: speciality_id };
+            const result = await newSpecialities.findOne(query);
+            res.send(result)
+        })
+
 
     }
     finally {
